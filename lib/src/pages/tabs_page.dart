e7033a890d075c85ef00f14241b5eb6329b519cf
DentVega <dentvega6@gmail.com>
2020-04-19 18:01:17 -0400

    - BottomNavigationBarItem(icon: Icon(Icons.person_outline), title: Text('data')),

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -4,18 +4,41 @@ class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: <Widget>[
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
-          ),
-        ],
-      ),
+      body: _Paginas(),
+      bottomNavigationBar: _Navegacion(),
     );
   }
 }
 
+class _Navegacion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(currentIndex: 1, items: [
+      BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline), title: Text('Para ti')),
+      BottomNavigationBarItem(
+          icon: Icon(Icons.public), title: Text('Encabezados')),
+    ]);
+  }
+}
+
+class _Paginas extends StatelessWidget {
+  const _Paginas({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+      ],
+    );
+  }
+}
